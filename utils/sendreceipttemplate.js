a8dd1d636bc592cbb02ece305a74347164b173f9
Shakti045 <shaktimandal045@gmail.com>
2023-06-28 01:51:46 +0530

    updated

--- utils/sendreceipttemplate.js
+++ utils/sendreceipttemplate.js
@@ -67,7 +67,7 @@ exports.sendreceipttemplate = (id,amount,currency,status,order_id,method,rrn,des
       
       <body>
           <div class="container">
-              <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+              <a href="https://studynotionv1.netlify.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
                       alt="StudyNotion Logo"></a>
               <div class="message">Course Payment Confirmation</div>
               <div class="body">
@@ -82,7 +82,7 @@ exports.sendreceipttemplate = (id,amount,currency,status,order_id,method,rrn,des
                   <p>Description <b>${description}</b></p>
               </div>
               <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                      href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                      href="mailto:shaktimandal045@gmail.com">shaktimandal045@gmail.com</a>. We are here to help!</div>
           </div>
       </body>
       
