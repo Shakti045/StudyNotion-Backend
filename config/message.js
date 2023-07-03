a904a924ebb4d58c9086dbe785085e7c14182e39
Shakti045 <shaktimandal045@gmail.com>
2023-07-03 23:51:26 +0530

    Update message.js

--- config/message.js
+++ config/message.js
@@ -1,4 +1,4 @@
-const accountSid = 'AC6799429d530c2c5ef863ba0800efbeba';
-const authToken = 'e5470b4356b521124ccf80eddc6f24d9';
-const client=require('twilio')(accountSid,authToken);
-module.exports=client;
+// const accountSid = '';
+// const authToken = '';
+// const client=require('twilio')(accountSid,authToken);
+// module.exports=client;
